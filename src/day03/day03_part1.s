a6fb1c6820f819be8baf190f55e413f00bb511a2
David Price <David.Price@experian.com>
2020-12-03 07:12:09 +0000

    created day 3 files, fixed duplicate write file

---
+++ src/day03/day03_part1.s
@@ -0,0 +1,49 @@
+
+.global _main
+
+# ------------------------------------------------------------- #
+# Variables
+.data
+inputsFileName: .asciz          "input.txt"
+fileDescriptor: .quad           4
+
+# Local Variables
+.bss
+fileBuffer:     .lcomm          fbuffer, 24000
+lineBuffer:     .lcomm          lbuffer, 64
+
+# ------------------------------------------------------------- #
+# Main
+.text
+_main:
+                # open the file and save descriptor
+                lea             rdi, [inputsFileName + RIP]     # move address of filename string to rdi
+                call            openfile
+                mov             [fileDescriptor + RIP], rax     # save returned file descriptor
+
+                # read the file to the buffer
+                mov             rax, [syscall_read + RIP]       # set sys call as read
+                mov             rdi, [fileDescriptor + RIP]     # file descriptor
+                lea             rsi, [fileBuffer + RIP]         # pointer to file buffer
+                mov             rdx, 24000                      # number of bytes to read
+                syscall
+
+                # Close a file
+                mov             rdi, [fileDescriptor + RIP]     # file descriptor of the opened file
+                call            closefile
+
+# ------------------------------------------------------------- #
+
+                # split buffer into lines and convert to integer, then push to stack
+                lea             rdx, [fileBuffer + RIP]         # buffer to split
+splitLoop:
+                lea             rdi, [lineBuffer + RIP]         # line output buffer
+                call            splitString
+
+
+
+
+
+
+                # Exit
+                call            exit
