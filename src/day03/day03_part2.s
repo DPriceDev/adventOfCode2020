4b71adc16954cedbcad46c227ea4c8908ff411fb
David Price <David.Price@experian.com>
2020-12-03 12:16:45 +0000

    neated day 03 part 2

--- src/day03/day03_part2.s
+++ src/day03/day03_part2.s
@@ -7,6 +7,9 @@
 inputsFileName: .asciz          "input.txt"
 fileDescriptor: .quad           4
 
+rows:           .int            1, 3, 5, 7, 1
+columns:        .int            1, 1, 1, 1, 2
+
 # Local Variables
 .bss
 fileBuffer:     .lcomm          fbuffer, 12000
@@ -44,153 +47,120 @@ _main:
                 # Get length of that row
                 lea             rsi,  [lineBuffer + RIP]
                 call            stringLength
-                mov             r10, rax
+                mov             r10, rax                        # store length of first row
 
+# ------------------------------------------------------------- #
+                # Setup loop variables
+                mov             rax, 1                          # set rax to 1 as a base multiple
+                mov             rbx, 5                          # Set loop of pairs counter to number of pairs
+                lea             rdi, [rows + RIP]               # set r14 and 415 to the row and columns addresses
+                lea             rcx, [columns + RIP]
+
+                # Loop through each set and find total
 multiplyLoop:
-                xor             rax, rax
+                # get row and column values
+                mov             r11w, [rdi]                     # row, move a word size as only want the integer
+                mov             r12w, [rcx]                     # column
 
-                push            rax
-                lea             rdx, [fileBuffer + RIP]         # Move rdx to start of input buffer
-                mov             r11, 1
-                mov             r12, 1
-                call            checkTrees
+                # check trees
+                lea             rsi, [fileBuffer + RIP]         # Move rdx to start of input buffer
+                call            checkTrees                      # check the number of trees with these steps, result in r9
 
-                mov             rax, r9
+                # multiply result
+                xor             rdx, rdx                        # clear rdx for the multiplication
+                mul             r9                              # multiply the current running total in rax by the number of trees in r9
 
-                push            rax
-                lea             rdx, [fileBuffer + RIP]         # Move rdx to start of input buffer
-                mov             r11, 3
-                mov             r12, 1
-                call            checkTrees
-                pop             rax
+                # decrement loop and go to next variable
+                dec             rbx                             # decrease loop count
+                add             rdi, 4                          # increment to the next integer (4 bytes/addresses)
+                add             rcx, 4
 
-                xor             rdx, rdx
-                mul             r9
+                # loop check
+                cmp             rbx, 0                          # loop if not finished
+                jne             multiplyLoop
 
-                push            rax
-                lea             rdx, [fileBuffer + RIP]         # Move rdx to start of input buffer
-                mov             r11, 5
-                mov             r12, 1
-                call            checkTrees
-                pop             rax
-
-                xor             rdx, rdx
-                mul             r9
-
-                push            rax
-                lea             rdx, [fileBuffer + RIP]         # Move rdx to start of input buffer
-                mov             r11, 7
-                mov             r12, 1
-                call            checkTrees
-                pop             rax
-
-                xor             rdx, rdx
-                mul             r9
-
-                push            rax
-                lea             rdx, [fileBuffer + RIP]         # Move rdx to start of input buffer
-                mov             r11, 1
-                mov             r12, 2
-                call            checkTrees
-                pop             rax
-
-                xor             rdx, rdx
-                mul             r9
-
+                # Print multiplication
                 call            printLnInteger
-
 
                 # Exit
                 call            exit
 
+
+
 # ------------------------------------------------------------- #
 # input rdx, pointer to buffer
 checkTrees:
-                push            r10
-
-                mov             r9, r10
-                mov             rsi, r10
-
-                # calculate rows to jump
                 push            rax
-                push            rdx
+                push            rbx
+                push            rcx
+                push            rsi
 
-                xor             rdx, rdx
-                inc             r10
+                # Clear counter and copy line length
+                xor             r9, r9                          # clear r9 as the tree counter
+                mov             rcx, r10                        # save original line length in r10
+                inc             rcx                             # add one to line length to account for new line char
 
-                mov             rax, r10
-                mul             r12
+                # calculate rows offset to jump
+                mov             rax, rcx                        # move line length (with nl) to rax
+                xor             rdx, rdx                        # clear rdx for multiplication
+                mul             r12                             # multiply line length by number of rows to skip down
+                mov             rcx, rax                        # update rax with length between desired rows
 
-                mov             r10, rax
+                mov             r8, rcx                         # save r8 as this length between desired rows
 
-                pop             rdx
-                pop             rax
+                # calculate tree offset
+                add             rcx, r11                        # add tree offset to rows to get total tree to tree distance
 
-                mov             r8, r10                         # save column offset in r8
-
-                # calculate the line offset
-                add             r10, r11
-
-                # calculate the check buffer i.e. 32 - 5~
-                mov             rax, r11
-                add             r11, r12                        # add the number of new lines to offset by
-                inc             rax                             # calculate back off from line end
+                # calculate how close to the end of a line before skipping a row
+                mov             rbx, r11
+                add             r11, r12
+                inc             rbx
 
                 # add row offset to r11
-                mov             r11, r10
+                mov             r11, rcx
                 sub             r11, r8
                 add             r11, r12                        # add the number of new lines to offset by
 
-                # clear buffers
-                mov             r12, r9
-                inc             r12
-                xor             r9, r9                          # clear r9
+                # set inital end of line to be a lines length
+                mov             r12, r10                        # move line length to r12
+                inc             r12                             # add one to account for new line character
 
 scanningLoop:
-                #
-                mov             r13, r12
-                sub             r13, rax                        # subtract 5 from the current line end count
+                # get the line end position and back off value
+                mov             r13, r12                        # move line end count to r13
+                sub             r13, rbx                        # subtract 5 from the current line end to get back off
 
-                #
-                mov             r14, rdx                        # copy the current buffer position
+                # Get the current position of the buffer ptr
+                mov             r14, rsi                        # copy the current buffer position
                 lea             r15, [fileBuffer + RIP]         # copy the start of the input buffer
                 sub             r14, r15                        # get length of rdx from start
 
                 # increment lines and line count
                 add             r12, r8                         # increment line counter
-                add             rdx, r10                        # add a line length + offset r11
+                add             rsi, rcx                        # add a line length + offset r11
 
                 # check if a line was lost due to end of line
                 cmp             r14, r13                        # check if that is close to the end of a line
                 jl              checkChar
-                sub             rdx, rsi
+                sub             rsi, r10                        # if close to the end, a line is skiped, subtract one line length
 
 checkChar:
-                push            r11
-                push            r10
-                push            r8
-                push            rax
-                push            rsi
-                mov             rsi, rdx
-                call            printChar                       # DEBUG
-                pop             rsi
-                pop             rax
-                pop             r8
-                pop             r10
-                pop             r11
-
                 # check if the char is null terminator, end if so
-                cmp             byte ptr [rdx], 0               # check for null terminator, if is null, finish loop
+                cmp             byte ptr [rsi], 0               # check for null terminator, if is null, finish loop
                 je              finished
 
                 # check if the char is a #
-                cmp             byte ptr [rdx], '#'             # check for tree, if not a tree, loop again
+                cmp             byte ptr [rsi], '#'             # check for tree, if not a tree, loop again
                 jne             scanningLoop
 
                 # increment and check next
-                inc             r9
+                inc             r9                              # increment number of trees
                 jmp scanningLoop
 finished:
-                pop             r10
+
+                pop             rsi
+                pop             rcx
+                pop             rbx
+                pop             rax
 
                 ret
