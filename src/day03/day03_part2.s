7067b5115adf00d2a3ea8cadb97c8f7c04bedced
David Price <David.Price@experian.com>
2020-12-03 10:34:34 +0000

    day 3 part 2 completed but quite messy

--- src/day03/day03_part2.s
+++ src/day03/day03_part2.s
@@ -46,13 +46,59 @@ _main:
                 call            stringLength
                 mov             r10, rax
 
+multiplyLoop:
+                xor             rax, rax
+
+                push            rax
                 lea             rdx, [fileBuffer + RIP]         # Move rdx to start of input buffer
-                mov             r11, 3
+                mov             r11, 1
+                mov             r12, 1
                 call            checkTrees
 
-                # print result
                 mov             rax, r9
-                call            printInteger
+
+                push            rax
+                lea             rdx, [fileBuffer + RIP]         # Move rdx to start of input buffer
+                mov             r11, 3
+                mov             r12, 1
+                call            checkTrees
+                pop             rax
+
+                xor             rdx, rdx
+                mul             r9
+
+                push            rax
+                lea             rdx, [fileBuffer + RIP]         # Move rdx to start of input buffer
+                mov             r11, 5
+                mov             r12, 1
+                call            checkTrees
+                pop             rax
+
+                xor             rdx, rdx
+                mul             r9
+
+                push            rax
+                lea             rdx, [fileBuffer + RIP]         # Move rdx to start of input buffer
+                mov             r11, 7
+                mov             r12, 1
+                call            checkTrees
+                pop             rax
+
+                xor             rdx, rdx
+                mul             r9
+
+                push            rax
+                lea             rdx, [fileBuffer + RIP]         # Move rdx to start of input buffer
+                mov             r11, 1
+                mov             r12, 2
+                call            checkTrees
+                pop             rax
+
+                xor             rdx, rdx
+                mul             r9
+
+                call            printLnInteger
+
 
                 # Exit
                 call            exit
@@ -60,47 +106,77 @@ _main:
 # ------------------------------------------------------------- #
 # input rdx, pointer to buffer
 checkTrees:
-                # work out the offset, line length + 3 + nl
+                push            r10
 
-                mov             rdi, r10        # copy line length, could push
+                mov             r9, r10
+                mov             rsi, r10
 
-                # setup line offset
+                # calculate rows to jump
+                push            rax
+                push            rdx
 
-                inc             r11
-                add             rdi, r11
+                xor             rdx, rdx
+                inc             r10
 
+                mov             rax, r10
+                mul             r12
+
+                mov             r10, rax
+
+                pop             rdx
+                pop             rax
+
+                mov             r8, r10                         # save column offset in r8
+
+                # calculate the line offset
+                add             r10, r11
+
+                # calculate the check buffer i.e. 32 - 5~
                 mov             rax, r11
-                inc             rax
+                add             r11, r12                        # add the number of new lines to offset by
+                inc             rax                             # calculate back off from line end
 
+                # add row offset to r11
+                mov             r11, r10
+                sub             r11, r8
+                add             r11, r12                        # add the number of new lines to offset by
+
+                # clear buffers
+                mov             r12, r9
+                inc             r12
                 xor             r9, r9                          # clear r9
-                xor             r12, r12                        # clear r12
-                mov             r13, rax                        # setup r13 with an offset of 5 to be subtracted on first loop
+
 scanningLoop:
-                add             r12, 32                        # increment line counter
+                #
                 mov             r13, r12
                 sub             r13, rax                        # subtract 5 from the current line end count
 
+                #
                 mov             r14, rdx                        # copy the current buffer position
                 lea             r15, [fileBuffer + RIP]         # copy the start of the input buffer
                 sub             r14, r15                        # get length of rdx from start
 
+                # increment lines and line count
+                add             r12, r8                         # increment line counter
+                add             rdx, r10                        # add a line length + offset r11
+
+                # check if a line was lost due to end of line
                 cmp             r14, r13                        # check if that is close to the end of a line
-                jle             addOffset
+                jl              checkChar
+                sub             rdx, rsi
 
-                add             rdx, r11                          # if close to a line end, only add 4
-
-                jmp             checkChar
-addOffset:
-                # add this offset to the file array pointer
-                add             rdx, rdi                        # add a line length + 4
 checkChar:
                 push            r11
-                push            r15
+                push            r10
+                push            r8
                 push            rax
+                push            rsi
                 mov             rsi, rdx
                 call            printChar                       # DEBUG
+                pop             rsi
                 pop             rax
-                pop             r15
+                pop             r8
+                pop             r10
                 pop             r11
 
                 # check if the char is null terminator, end if so
@@ -115,4 +191,6 @@ checkChar:
                 inc             r9
                 jmp scanningLoop
 finished:
+                pop             r10
+
                 ret
