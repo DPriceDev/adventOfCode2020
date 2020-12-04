a7cb9acaea862234ce6630d5838c8b7cb41197c3
David Price <David.Price@experian.com>
2020-12-04 07:23:25 +0000

    Added day 4 files

---
+++ src/day04/day04_part1.s
@@ -0,0 +1,44 @@
+
+.global _main
+
+# ------------------------------------------------------------- #
+# Variables
+.data
+inputsFileName: .asciz          "input.txt"
+fileDescriptor: .quad           4
+fileSize:       .int            20000
+
+# Local Variables
+.bss
+fileBuffer:     .lcomm          fbuffer, 20000
+lineBuffer:     .lcomm          lbuffer, 64
+
+# ------------------------------------------------------------- #
+# Main
+.text
+_main:
+                # open the file and save descriptor
+                lea             rdi, [inputsFileName + RIP]     # move address of filename string to rdi
+                call            openfile
+                mov             [fileDescriptor + RIP], rax     # save returned file descriptor
+
+                # read the file to the buffer
+                mov             rax, [syscall_read + RIP]       # set sys call as read
+                mov             rdi, [fileDescriptor + RIP]     # file descriptor
+                lea             rsi, [fileBuffer + RIP]         # pointer to file buffer
+                mov             rdx, 20000                      # number of bytes to read
+                syscall
+
+                # Close a file
+                mov             rdi, [fileDescriptor + RIP]     # file descriptor of the opened file
+                call            closefile
+
+# ------------------------------------------------------------- #
+
+                # Get first row
+                lea             rdx, [fileBuffer + RIP]         # buffer to split
+                lea             rdi, [lineBuffer + RIP]         # line output buffer
+                call            splitString
+
+                # Exit
+                call            exit
