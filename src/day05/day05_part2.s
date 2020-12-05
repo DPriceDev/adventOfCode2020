7ccf12201ed75d2dff2d1b75e6b754582a3dc6a7
David Price <David.Price@experian.com>
2020-12-05 10:13:45 +0000

    neated solution

--- src/day05/day05_part2.s
+++ src/day05/day05_part2.s
@@ -12,7 +12,7 @@ splitToken:     .asciz          "\n"
 .bss
 fileBuffer:     .lcomm          fbuffer, 24000
 lineBuffer:     .lcomm          lbuffer, 24
-idBuffer:       .lcomm          ibuffer, 3568
+idBuffer:       .lcomm          ibuffer, 892
 
 # ------------------------------------------------------------- #
 # Main
@@ -38,111 +38,93 @@ _main:
                 lea             rdx, [fileBuffer + RIP]         # buffer to split
                 xor             r11, r11
 splitLoop:
-                cmp             byte ptr [rdx], 0
+                cmp             byte ptr [rdx], 0               # check finished parsing file
                 je              finished
 
-                push            r11
+                /* Get next input line */
+                push            r11                             # save r11 register
                 lea             rdi, [lineBuffer + RIP]         # line output buffer
-                lea             rsi, [splitToken + RIP]         # pointer to token string
-                call            splitStringToken
-                lea             rdi, [lineBuffer + RIP]
-                pop             r11
+                lea             rsi, [splitToken + RIP]         # pointer to new line token string
+                call            splitStringToken                # split the string by new lines
+                pop             r11                             # restore r11 register
 
-                // do binary check to get ID
+                /* Get the ID from the binary input */
+                lea             rdi, [lineBuffer + RIP]         # go to beginning of the line buffer
                 call            binaryCheck
 
-                // save id to array
-                shl             rax, 2
-                lea             rsi, [idBuffer + RIP]
-                add             rsi, rax
-                shr             rax, 2
-                mov             word ptr [rsi], ax
+                /* save a true false value to its position in the array */
+                lea             rsi, [idBuffer + RIP]           # get the start of the id array
+                add             rsi, rax                        # offset the array pointer by the id
+                mov             byte ptr [rsi], 1               # set the byte to 1 (true)
 
-                // get the highest ID
-                cmp             r11, rax
-                jge             splitLoop
-                mov             r11, rax
-
-                jmp             splitLoop
+                /* Compare the current highest to the id, move if higher */
+                cmp             rax, r11                        # compare the id to the running highest
+                jl              splitLoop                       # loop when rax is lower than r11
+                mov             r11, rax                        # or update new highest id
+                jmp             splitLoop                       # loop back to new input line
 finished:
-                lea             rsi, [idBuffer + RIP]
-                call            scanForSeat
+                lea             rsi, [idBuffer + RIP]           # move the id list to rsi and
+                call            scanForSeat                     # find the seat
 
-                // finished
-                mov             rax, r11
+                /* Print result */
+                mov             rax, r11                        # print highest ID
                 call            printLnInteger
-
-                mov             rax, r12
+                mov             rax, r12                        # print seat number
                 call            printInteger
 
-                # Exit
+                /* Exit Program */
                 call            exit
 
 # ------------------------------------------------------------- #
 binaryCheck:
-                mov             r9, 64
-                mov             r10, 4
-                xor             rax, rax
+                mov             r9, 64                          # set the rows bits to be 1000000b
+                mov             r10, 4                          # set the rows bits to be 0000100b
+                xor             rax, rax                        # clear registers
                 xor             rbx, rbx
 binaryCheck_loop:
-                // F sets the binary value to 1
-                cmp             byte ptr [rdi], 'B'
+                cmp             byte ptr [rdi], 'B'             # check for B
                 je              binaryCheck_B
-
-                // B sets the binary value to 0
-                cmp             byte ptr [rdi], 'F'
+                cmp             byte ptr [rdi], 'F'             # check for F
                 je              binaryCheck_F
-
-                // R sets the binary value to 1
-                cmp             byte ptr [rdi], 'R'
+                cmp             byte ptr [rdi], 'R'             # check for R
                 je              binaryCheck_R
-
-                // L sets the binary value to 0
-                cmp             byte ptr [rdi], 'L'
+                cmp             byte ptr [rdi], 'L'             # check for L
                 je              binaryCheck_L
-
-                jmp             binaryCheckLoop_finished
+                jmp             binaryCheckLoop_finished        # when none of these, finished the row
 binaryCheck_B:
-                add             rax, r9
+                add             rax, r9                         # when higher column (B) add the bit value
 binaryCheck_F:
-                shr             r9
+                shr             r9                              # shift the register i.e. 1000000b -> 0100000b
                 jmp             binaryCheck_increment
 binaryCheck_R:
-                add             rbx, r10
+                add             rbx, r10                        # when higher column (B) add the bit value
 binaryCheck_L:
-                shr             r10
-
+                shr             r10                             # shift the register i.e. 0000100b -> 0000010b
 binaryCheck_increment:
-                inc             rdi
+                inc             rdi                             # move to next char in line
                 jmp             binaryCheck_loop
 binaryCheckLoop_finished:
-                // on row completion, multiply row and column
-                shl             rax, 3
-                add             rax, rbx
-
+                shl             rax, 3                          # multiply by 8
+                add             rax, rbx                        # add the column
                 ret
 
 # ------------------------------------------------------------- #
-scanForSeat_loop:
-                add             rsi, 4
 scanForSeat:
-                lea             rcx, [idBuffer + RIP + 3568]
-                cmp             rsi, rcx
+                lea             rcx, [idBuffer + RIP]           # check if the end of the array is reached
+                add             rcx, r11
+                jmp             scanForSeat_start
+scanForSeat_loop:
+                inc             rsi                             # move to next array location
+scanForSeat_start:
+                cmp             rsi, rcx                        # check end of the array is reached
                 jge             scanForSeat_finished
-
-                cmp             word ptr [rsi], 0
+                cmp             byte ptr [rsi], 0               # check this byte is false
                 jne             scanForSeat_loop
-
-                cmp             word ptr [rsi - 4], 0
-                je              scanForSeat_loop
-
-                cmp             word ptr [rsi + 4], 0
+                cmp             byte ptr [rsi - 1], 0           # check previous byte is true
                 je              scanForSeat_loop
 
 scanForSeat_finished:
-                mov             r12, rsi
-                lea             rsi, [idBuffer + RIP]
-                sub             r12, rsi
-                shr             r12, 2
-
+                mov             r12, rsi                        # save the current address position
+                lea             rsi, [idBuffer + RIP]           # get the start address
+                sub             r12, rsi                        # subtract to get the id number
                 ret
