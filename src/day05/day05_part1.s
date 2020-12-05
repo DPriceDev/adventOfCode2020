577cdd9215926f9ce0b078cb3d091ff30ab043c3
David Price <David.Price@experian.com>
2020-12-05 08:43:05 +0000

    part 1 complete

--- src/day05/day05_part1.s
+++ src/day05/day05_part1.s
@@ -7,14 +7,11 @@
 inputsFileName: .asciz          "input.txt"
 fileDescriptor: .quad           4
 fileSize:       .quad           24000
+splitToken:     .asciz          "\n"
 
-groupToken:     .asciz          "\n\n"
-passportTokens: .asciz          "byr:", "iyr:", "eyr:", "hgt:", "hcl:", "ecl:", "pid:"
-
-# Local Variables
 .bss
 fileBuffer:     .lcomm          fbuffer, 24000
-lineBuffer:     .lcomm          lbuffer, 248
+lineBuffer:     .lcomm          lbuffer, 24
 
 # ------------------------------------------------------------- #
 # Main
@@ -37,20 +34,72 @@ _main:
                 call            closefile
 
 # ------------------------------------------------------------- #
-                # split string by the token "\n\n"
                 lea             rdx, [fileBuffer + RIP]         # buffer to split
+                xor             r11, r11
 splitLoop:
-                lea             rdi, [lineBuffer + RIP]         # line output buffer
-                lea             rsi, [groupToken + RIP]         # pointer to token string
-                call            splitStringToken
-
-                // use string
-
-                # check for terminator in file buffer
                 cmp             byte ptr [rdx], 0
-                jne             splitLoop
+                je              finished
 
+                push            r11
+                lea             rdi, [lineBuffer + RIP]         # line output buffer
+                lea             rsi, [splitToken + RIP]         # pointer to token string
+                call            splitStringToken
+                lea             rdi, [lineBuffer + RIP]
+                pop             r11
+
+                # setup values
+                mov             r9, 64
+                mov             r10, 4
+                xor             rax, rax
+                xor             rbx, rbx
+binaryCheckLoop:
+                // F sets the binary value to 1
+                cmp             byte ptr [rdi], 'B'
+                je              binaryCheck_B
+
+                // B sets the binary value to 0
+                cmp             byte ptr [rdi], 'F'
+                je              binaryCheck_F
+
+                // R sets the binary value to 1
+                cmp             byte ptr [rdi], 'R'
+                je              binaryCheck_R
+
+                // L sets the binary value to 0
+                cmp             byte ptr [rdi], 'L'
+                je              binaryCheck_L
+
+                jmp             binaryCheckLoop_finished
+binaryCheck_B:
+                add             rax, r9
+binaryCheck_F:
+                shr             r9
+                jmp             binaryCheck_increment
+binaryCheck_R:
+                add             rbx, r10
+binaryCheck_L:
+                shr             r10
+
+binaryCheck_increment:
+                inc             rdi
+                jmp             binaryCheckLoop
+binaryCheckLoop_finished:
+                // on row completion, multiply row and column
+                shl             rax, 3
+                add             rax, rbx
+
+                // compare to current highest, replace if higher
+                cmp             r11, rax
+                jge             splitLoop
+
+                mov             r11, rax
+
+                jmp             splitLoop
+
+finished:
                 // finished
+                mov             rax, r11
+                call            printInteger
 
                 # Exit
                 call            exit
