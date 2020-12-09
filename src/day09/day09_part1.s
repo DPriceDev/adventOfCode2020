65c42025d45eef8ad96509f14fcf92332ba7c527
David Price <David.Price@experian.com>
2020-12-09 07:51:11 +0000

    create starting files

---
+++ src/day09/day09_part1.s
@@ -0,0 +1,38 @@
+
+.global _main
+
+.include "macros.s"
+
+# ------------------------------------------------------------- #
+# Variables
+.data
+inputsFileName: .asciz          "input.txt"
+fileDescriptor: .quad           4
+fileSize:       .quad           10000
+splitToken:     .asciz          "\n"
+
+.bss
+fileBuffer:     .lcomm          fbuffer, 10000
+lineBuffer:     .lcomm          lbuffer, 16
+answerBuffer:   .lcomm          abuffer, 26
+
+# ------------------------------------------------------------- #
+# Main
+.text
+_main:
+                oftb            [inputsFileName + RIP], [fileBuffer + RIP], [fileSize + RIP]
+# ------------------------------------------------------------- #
+                lea             rdx, [fileBuffer + RIP]
+                xor             r11, r11
+splitLoop:
+                cmp             byte ptr [rdx], 0               // check for null, finished when no more lines
+                je              finished
+
+                ssbt            [lineBuffer + RIP], [splitToken + RIP]
+
+                inc             r11
+                jmp             splitLoop
+finished:
+                mov             rax, r11
+                call            printInteger                    // print result
+                call            exit
